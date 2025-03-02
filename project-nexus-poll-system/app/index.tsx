cc3439a70a6444a7d55bdbd616711ec6ba0cc3a3
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2025-03-02 13:23:53 +0400

    fix : tailwind now working properly

--- project-nexus-poll-system/app/index.tsx
+++ project-nexus-poll-system/app/index.tsx
@@ -1,24 +1,28 @@
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 import { Link } from 'expo-router';
+import '../global.css';
 
 export default function WelcomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center p-4 bg-gray-100">
-      <Text>Nexus Poll 🗳️</Text>
-      
-      <View>
-        <Link href="/(tabs)/create" asChild>
-          <Button
-            title="Create New Poll"
-          />
-        </Link>
+    <ImageBackground source={require('../assets/images/wave.jpeg')} className="flex-1 justify-center items-center">
+      <View className="flex-1 justify-center items-center p-4 bg-white bg-opacity-80">
+        <Text className="text-4xl font-bold text-center mb-6">Welcome{'\n'}to{'\n'}Nexus Poll</Text>
         
-        <Link href="/(tabs)/vote" asChild>
-          <Button
-            title="Vote in Polls"
-          />
-        </Link>
+        <View className="flex-row justify-between w-full px-4">
+          <Link href="/(tabs)/create" asChild>
+            <TouchableOpacity className="bg-purple-600 rounded-full mx-2 py-2 px-4">
+              <Text className="text-white text-center">Create a Poll</Text>
+            </TouchableOpacity>
+          </Link>
+          
+          <Link href="/(tabs)/vote" asChild>
+            <TouchableOpacity className="bg-purple-600 rounded-full mx-2 py-2 px-4">
+              <Text className="text-white text-center">Vote for a Poll</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
